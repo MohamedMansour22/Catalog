59d4911aa52c4de57bc9a8e62eecf83ef69a28f3
MohamedMansour22 <46927112+MohamedMansour22@users.noreply.github.com>
2019-06-25 13:39:48 -0700

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -58,6 +58,8 @@ class Game(Base):
         }
 
 
-engine = create_engine('sqlite:///catalog.db')
+# engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
+
 
 Base.metadata.create_all(engine)
